7ce0706da25ebb293e55d26b6130011a61deea72
abarnaramaraj <47477481+abarnaramaraj@users.noreply.github.com>
2019-03-04 14:43:19 +0530

    Create words.c

---
+++ words.c
@@ -0,0 +1,15 @@
+void main()
+{
+char a[100]
+int len,i,word=0;
+clrscr();
+print("enter the  string is");
+len=strlen(a);
+for(i=1;i<=len,i++)
+{
+if(a[i]=''&&a[i+1]='')
+{
+print("the lenth of string is",word)
+++word
+getch();
+}
